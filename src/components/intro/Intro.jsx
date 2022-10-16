4d5ebca79c76fb113ab8406fd8ab44a8699e262a
Charles Callender <charlescallender.cc@gmail.com>
2022-10-15 21:23:32 -0700

    Added chess gif

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -10,6 +10,8 @@ export default function Intro() {
     const theme = useContext(ThemeContext)
     const darkMode = theme.state.darkMode;
 
+    const src1 = "https://images.chesscomfiles.com/uploads/game-gifs/90px/brown/neo/0/cc/0/1/bUNZUW5EWkpDSlFKbEIhVGd2NkVrczBTZkg1WnB4RU5vRU5VdktXT0h5WEh5a1pLREtUWmVnN0ZrVUZ3Z2gzVWR2P3g,.gif";
+
     useEffect(() => {
         init(textRef.current, {
             showCursor: true,
@@ -24,11 +26,25 @@ export default function Intro() {
         <div className='intro' id='intro'>
             <div className="left">
                 <div className="imgContainer">
-                    <img src={process.env.PUBLIC_URL + '/assets/chess1.svg'} alt="">
+                    {/* <img src={process.env.PUBLIC_URL + '/assets/chess1.svg'} alt="">
                     
 			
 	
-                    </img>
+                    </img> */}
+                    {/* <video className="video1" controls width="100%" autoPlay>
+                    <source src={src1} type="video/mp4" />
+
+                    </video> */}
+                    <div className="video1">
+                    <iframe
+                        src={src1}
+                        title="Youtube Player"
+                        frameBorder="0"
+                        className="video1"
+                        scrolling="no"
+                        
+                        />
+                        </div>
                 </div>
             </div>
             <div className="right">
