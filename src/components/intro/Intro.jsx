436fee26d556582d15e89d22e779179799edd125
Charles Callender <charlescallender.cc@gmail.com>
2022-10-16 21:39:03 -0700

    Added in ability to play multiple gifs

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,14 +3,45 @@ import { init } from 'ityped';
 import { useEffect, useRef, useContext } from "react";
 import ArrowDropDownCircleOutlinedIcon from '@mui/icons-material/ArrowDropDownCircleOutlined';
 import { ThemeContext } from "../../context";
+import { useState } from "react";
 
 export default function Intro() {
 
     const textRef = useRef();
     const theme = useContext(ThemeContext)
     const darkMode = theme.state.darkMode;
+    const [timePlaying, setTimePlaying] = useState(0)
+    const [gifNumber, setGifNumber] = useState(0)
 
-    const src1 = "https://images.chesscomfiles.com/uploads/game-gifs/90px/brown/neo/0/cc/0/1/bUNZUW5EWkpDSlFKbEIhVGd2NkVrczBTZkg1WnB4RU5vRU5VdktXT0h5WEh5a1pLREtUWmVnN0ZrVUZ3Z2gzVWR2P3g,.gif";
+    
+
+    const gifSrc = [
+        "https://images.chesscomfiles.com/uploads/game-gifs/90px/brown/neo/0/cc/0/1/bUNZUW5EWkpDSlFKbEIhVGd2NkVrczBTZkg1WnB4RU5vRU5VdktXT0h5WEh5a1pLREtUWmVnN0ZrVUZ3Z2gzVWR2P3g,.gif",
+        "https://images.chesscomfiles.com/uploads/game-gifs/90px/brown/neo/0/cc/0/0/bEIwU2NEWkptdSFUa3NZSWJsSUJ1QjVRZ3Y5UkR3UndwdzghZnQ3WWRtOTh2S1FLQktUQ2xDSkNtQzZaQzMhOTM_OTA_MllJMlQwOWg_.gif"
+    ]
+
+    const myGifs = gifSrc.map((source) => 
+    (
+    <div className="video1">
+                    <iframe
+                        src={source}
+                        title="Youtube Player"
+                        frameBorder="0"
+                        className="video1"
+                        scrolling="no"
+                        
+                        />
+                        </div>
+                
+    )
+    )
+    console.log({myGifs})
+
+    // const nextGif = () => {
+    //     for (let i = 0; i < myGifs.length; i++)
+            // setTimePlaying(myGifs[i]._owner.actualDuration)
+    //     if (myGifs[i]._owner.actualDuration)
+    // }
 
     useEffect(() => {
         init(textRef.current, {
@@ -22,10 +53,28 @@ export default function Intro() {
 
     }, []);
 
+    const nextVid = () => {
+        setGifNumber((prevState) => prevState + 1)
+        console.log(gifNumber)
+    }
+
+    const prevVid = () => {
+        setGifNumber((prevState) => prevState - 1)
+        console.log(gifNumber)
+    }
+
     return (
         <div className='intro' id='intro'>
             <div className="left">
                 <div className="imgContainer">
+                <button
+                type="button"
+                onClick={nextVid}
+            >Next</button>
+            <button
+                type="button"
+                onClick={prevVid}
+            >Prev</button>
                     {/* <img src={process.env.PUBLIC_URL + '/assets/chess1.svg'} alt="">
                     
 			
@@ -35,7 +84,7 @@ export default function Intro() {
                     <source src={src1} type="video/mp4" />
 
                     </video> */}
-                    <div className="video1">
+                    {/* <div className="video1">
                     <iframe
                         src={src1}
                         title="Youtube Player"
@@ -44,7 +93,8 @@ export default function Intro() {
                         scrolling="no"
                         
                         />
-                        </div>
+                        </div> */}
+                        {myGifs[gifNumber]}
                 </div>
             </div>
             <div className="right">
